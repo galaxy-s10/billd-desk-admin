2f91f9cc752606a7c4df172442765dfa184bfaf0
shuisheng <2274751790@qq.com>
2024-12-23 22:59:39 +0800

    feat: init

---
+++ src/global.d.ts
@@ -0,0 +1,8 @@
+interface Window {
+  $message: import('naive-ui/es/message/src/MessageProvider').MessageApiInjection;
+  $modal: import('naive-ui/es/modal/src/ModalProvider').ModalApiInjection;
+}
+
+declare global {
+  // import utils from 'billd-utils';
+}
