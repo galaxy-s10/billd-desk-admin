9c37218c73c7cb56b0243f5539b2e4e235013c5c
shuisheng <2274751790@qq.com>
2025-02-07 20:30:20 +0800

    fix: 保存

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig(({ mode }) => {
 
   const outputStaticUrl = () => {
     if (isProduction) {
-      return 'https://resource.hsslive.cn/billd-desk/admin/dist/';
+      return 'https://tencentcos-res.hsslive.cn/billd-desk-admin/client/dist/';
     } else {
       return './';
     }
